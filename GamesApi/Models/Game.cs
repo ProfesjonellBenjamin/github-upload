835d79df9a9346ad02d253c9acb429446b996679
Benjamin Tornes <benjamin.tornes@gmail.com>
2020-11-26 22:23:37 +0100

    axios react added

--- GamesApi/Models/Game.cs
+++ GamesApi/Models/Game.cs
@@ -10,7 +10,7 @@ namespace GamesApi.Models{
         public string Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
-        
+        public string Image { get; set; }
 
     }
 
--- gamespage/src/App.js
+++ gamespage/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import AllGames from './views/AllGames';
+import CreateGame from './views/CreateGame';
+import DeleteGames from './views/DeleteGames';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+        <ul>
+          <li><Link to ="/">All Games</Link></li>
+          <li><Link to ="/create-game">Create Game</Link></li>
+          <li><Link to ="/delete-game">Delete Game</Link></li>
+          
+        </ul>
+        <Switch>
+          <Route exact path="/" component = {AllGames}></Route>
+          <Route path="/create-game" component = {CreateGame}></Route>
+          <Route path="/delete-game" component = {DeleteGames}></Route>
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
