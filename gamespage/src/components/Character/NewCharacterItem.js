79a7044bbe727345c87b009325ece6448539ad17
Benjamin Tornes <benjamin.tornes@gmail.com>
2020-12-04 04:40:12 +0100

    final???

--- gamespage/src/components/Character/NewCharacterItem.js
+++ gamespage/src/components/Character/NewCharacterItem.js
@@ -1,6 +1,6 @@
 import { CharacterContext } from "../../contexts/CharacterContext";
-import {useContext} from 'react';
-import { Col, Card, Button } from 'react-bootstrap';
+import {useContext, useState} from 'react';
+import { Col, Card, Button, Modal, ButtonGroup } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 
 const NewCharacterItem = ( {id, name, description, image} ) => {
@@ -9,18 +9,54 @@ const NewCharacterItem = ( {id, name, description, image} ) => {
     const [characterState, setCharacter] = character;
 
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {
+      
+      setShow(false);
+
+  
+    }
+
+    const readMore = () => setShow(true);
+
     return (
 
         <Col xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 } xl={ 2 }> 
-    <Card style={{ width:'15rem', height:'25rem' }}>
-    <Image style={{ height:'15rem' }} variant ="top" src={`https://localhost:5001/images/${image}`} thumbnail />
-        <Card.Body>
-            <Card.Title as="h4">{name}</Card.Title>
-            <Card.Text>{description}</Card.Text>
-        </Card.Body>
-        <Button >Read More!</Button>
+            <Card style={{ width:'15rem', height:'28rem' }}>
+            <Image style={{ height:'15rem' }} variant ="top" src={`https://localhost:5001/images/${image}`} thumbnail />
+                <Card.Body>
+                    <Card.Title as="h4">{name}</Card.Title>
+                </Card.Body>
+                <ButtonGroup aria-label="Basic example">
+                    <Button variant="secondary" onClick={readMore}>Read More</Button>
+
+                </ButtonGroup>
     </Card>
-</Col>
+
+    <Modal animation={false} size="lg" show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                </Modal.Header>
+                <Modal.Body>
+                    
+                <Card className ="m-auto" style={{ width:'30rem', height:'28rem' }}>
+                            <Image style={{ height:'15rem' }} variant ="top" src={`https://localhost:5001/images/${image}`} thumbnail />
+                        <Card.Body>
+                            <Card.Title as="h4" className="center">{name}</Card.Title>
+                             <Card.Text>{description}</Card.Text>
+                        </Card.Body>
+                    </Card>
+
+
+                </Modal.Body>
+                <Modal.Footer>
+
+                <Button variant="primary" onClick={handleClose}>
+                    Close
+                </Button>
+                </Modal.Footer>
+            </Modal>
+        </Col>
 
 
     )
--- gamespage/src/components/Game/NewGameItem.js
+++ gamespage/src/components/Game/NewGameItem.js
@@ -1,6 +1,6 @@
 import { GameContext } from "../../contexts/GameContext";
-import {useContext} from 'react';
-import { Col, Card, Button } from 'react-bootstrap';
+import {useContext, useState} from 'react';
+import { Col, Card, Button, Modal, ButtonGroup } from 'react-bootstrap';
 import axios from 'axios';
 import Image from 'react-bootstrap/Image'
 
@@ -9,23 +9,60 @@ const NewGameItem = ( {id, name, price, image, console, description} ) => {
     const { game } = useContext(GameContext);
     const [gameState, setGame] = game;
 
+       //modal
+       const [show, setShow] = useState(false);
+
+       const handleClose = () => {
+         
+         setShow(false);
    
+     
+       }
+   
+       const readMore = () => setShow(true);
 
     
     return (
 
 
+
 <Col xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 } xl={ 2 }> 
-    <Card style={{ width:'15rem', height:'25rem' }}>
+    <Card style={{ width:'15rem', height:'28rem' }}>
     <Image style={{ height:'15rem' }} variant ="top" src={`https://localhost:5001/images/${image}`} thumbnail />
         <Card.Body>
             <Card.Title as="h4">{name}</Card.Title>
             <Card.Text>{price},- </Card.Text>
+            <Card.Text>{console}</Card.Text>
         </Card.Body>
-        <Button >Read More!</Button>
+        <ButtonGroup aria-label="Basic example">
+            <Button variant="secondary" onClick={readMore}>Read More</Button>
+
+        </ButtonGroup>
     </Card>
+
+    <Modal animation={false} size="lg" show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                </Modal.Header>
+                <Modal.Body>
+                    
+                <Card className ="m-auto" style={{ width:'30rem', height:'28rem' }}>
+                            <Image style={{ height:'15rem' }} variant ="top" src={`https://localhost:5001/images/${image}`} thumbnail />
+                        <Card.Body>
+                            <Card.Title as="h4" className="center">{name}</Card.Title>
+                             <Card.Text>{description}</Card.Text>
+                        </Card.Body>
+                    </Card>
+
+
+                </Modal.Body>
+                <Modal.Footer>
+
+                <Button variant="primary" onClick={handleClose}>
+                    Close
+                </Button>
+                </Modal.Footer>
+            </Modal>
 </Col>
-    
 
  
 
